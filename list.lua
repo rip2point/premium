337487a8d6b11546d63491400d446c1879720f6d
xurel <93357496+rip2point@users.noreply.github.com>
2023-10-10 18:54:17 -0400

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,20 @@
+local users = {
+	["Premium"] = {
+		[1234567890] = "nil"
+	},
+	["Admin"] = {
+		[759881255] = "true"	
+	}
+}
+
+
+local function checkPremium(userid)
+	for rank, tbl in next, users do
+		for id, state in next, tbl do
+			if id == userid then
+				return rank.." "..state
+			end
+		end
+	end
+	return
+end
